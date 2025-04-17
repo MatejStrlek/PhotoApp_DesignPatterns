0605fe82373e07e1dd2f30dfab2986684b506ebe
Matej Galic <matej.galic.ets@gmail.com>
2025-04-17 14:01:29 +0200

    init

---
+++ src/main/java/hr/algebra/photoapp_designpatterns_galic/PhotoAppDesignPatternsGalicApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.photoapp_designpatterns_galic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppDesignPatternsGalicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppDesignPatternsGalicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/algebra/photoapp_designpatterns_galic/PhotoAppDesignPatternsGalicApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.photoapp_designpatterns_galic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDesignPatternsGalicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
