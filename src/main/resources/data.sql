5473eb6be87fd502f52e76e4a4c671a3ab2c1e3e
Matej Galic <matej.galic.ets@gmail.com>
2025-05-09 12:38:13 +0200

    modified db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,5 @@ INSERT INTO app_user (email, password, role_user, package_type, auth_provider)
 VALUES
     ('admin@example.com', '$2a$10$xszlj1pbFnz6kKreb3X9pugjNUg0MAcC1ff/NR43oKNthJYYcCRj6', 'ADMIN', 'GOLD', 'LOCAL'),
     ('user1@example.com', '$2a$10$xszlj1pbFnz6kKreb3X9pugjNUg0MAcC1ff/NR43oKNthJYYcCRj6', 'REGISTERED', 'PRO', 'LOCAL'),
-    ('user2@example.com', '$2a$10$xszlj1pbFnz6kKreb3X9pugjNUg0MAcC1ff/NR43oKNthJYYcCRj6', 'REGISTERED', 'FREE', 'LOCAL');
+    ('user2@example.com', '$2a$10$xszlj1pbFnz6kKreb3X9pugjNUg0MAcC1ff/NR43oKNthJYYcCRj6', 'REGISTERED', 'FREE', 'LOCAL'),
+    ('user3@example.com', '$2a$10$xszlj1pbFnz6kKreb3X9pugjNUg0MAcC1ff/NR43oKNthJYYcCRj6', 'REGISTERED', NULL, 'LOCAL');
