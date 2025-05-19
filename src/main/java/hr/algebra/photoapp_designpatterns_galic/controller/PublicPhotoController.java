d3d9abc3b5bd59f018e81be2b9eda3a8ade7943d
Matej Galic <matej.galic.ets@gmail.com>
2025-05-19 11:33:18 +0200

    added download actions for original and processed photos

--- src/main/java/hr/algebra/photoapp_designpatterns_galic/controller/PublicPhotoController.java
+++ src/main/java/hr/algebra/photoapp_designpatterns_galic/controller/PublicPhotoController.java
@@ -1,15 +1,22 @@
 package hr.algebra.photoapp_designpatterns_galic.controller;
 
+import org.springframework.core.io.Resource;
 import hr.algebra.photoapp_designpatterns_galic.dto.PhotoSearchDTO;
 import hr.algebra.photoapp_designpatterns_galic.model.Photo;
 import hr.algebra.photoapp_designpatterns_galic.service.PhotoShowService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
@@ -42,5 +49,34 @@ public class PublicPhotoController {
         List<Photo> filteredPhotos = photoShowService.searchPhotos(filter);
         model.addAttribute("photos", filteredPhotos);
         return "public-photos";
+    }
+
+    @GetMapping("/public/photos/download/original/{id}")
+    public ResponseEntity<Resource> downloadOriginalPhoto(@PathVariable Long id) throws IOException {
+        return downloadFile(id, true);
+    }
+
+    @GetMapping("/public/photos/download/processed/{id}")
+    public ResponseEntity<Resource> downloadProcessedPhoto(@PathVariable Long id) throws IOException {
+        return downloadFile(id, false);
+    }
+
+    private ResponseEntity<Resource> downloadFile(@PathVariable Long id, boolean original) throws IOException {
+        Photo photo = photoShowService.findPhotoById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Photo not found"));
+
+        String photoPath = original ? photo.getOriginalFilePath() : photo.getProcessedFilePath();
+        String photoName = original ? photo.getOriginalFileName() : photo.getProcessedFileName();
+
+        Path path = Paths.get(photoPath);
+        if (!path.toFile().exists()) {
+            throw new IOException("File not found");
+        }
+
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=\"" + photoName + "\"")
+                .contentLength(resource.contentLength())
+                .body(resource);
     }
 }
